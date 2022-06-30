f0acd7c5c711245819aa2819b1c159c835d8057a
박승준 <vndan110@naver.com>
2022-06-30 09:01:28 +0900

    20220630

--- src/java_personal/Hello.java
+++ src/java_personal/Hello.java
@@ -5,5 +5,12 @@ public class Hello {
 	public static void main(String[] args) {
 		// 
 		System.out.println("Hello World !!!");
+		
+		int a=2;
+		int b=3;
+		
+		
+		System.out.println(a+b);
+		
 	}//main() end
 }//class end
