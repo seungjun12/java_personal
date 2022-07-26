a72573eeb02a3b5f95517e13adbe6f8d83bd37fa
seungjun12 <vndan110@naver.com>
2022-07-26 16:16:37 +0900

    dd

--- src/java_personal/Hello.java
+++ src/java_personal/Hello.java
@@ -4,23 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// 
-		System.out.println("Hello World !!!");
-		
-		int a=2;
-		int b=3;
-		int c=3;
-		System.out.println(c+a);
-		//확인 확인 한번더
-		System.out.println(a+b);
-		
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println(1);
-		System.out.println("hello");
-		
-		
-		
-		
+		System.out.println("HELLO WORLD!!");
 	}//main() end
+
 }//class end
